6f19a0a126a35a8948353e3ac852e9aad964085f
Juraj Polák <xpolak40@vutbr.cz>
2022-07-22 10:41:10 +0200

    Callable functions added

--- fr_modul.py
+++ fr_modul.py
@@ -8,72 +8,75 @@ import cv2
 casc_path = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(casc_path)
 
-# # From video feed
-# video = cv2.VideoCapture(0)
+# From video feed
+def video_detection():
+    video = cv2.VideoCapture(0)
 
-# while True:
-#     # Frame by frame capture
-#     ret, frames = video.read()
+    while True:
+        # Frame by frame capture
+        ret, frames = video.read()
 
-#     # Convertion to greyscale
-#     grey = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+        # Convertion to greyscale
+        grey = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
-#     faces = face_cascade.detectMultiScale(
-#         grey,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30,30),
-#         flags=cv2.CASCADE_SCALE_IMAGE
-#     )
+        faces = face_cascade.detectMultiScale(
+            grey,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30,30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
 
-#     # Highlight faces
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Highlight faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-#     # Dispaly video
-#     cv2.imshow('Video', frames)
+        # Dispaly video
+        cv2.imshow('Video', frames)
 
-#     # Exit condition
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+        # Exit condition
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# video.release()
-# cv2.destroyAllWindows()
-
-KNOWN_FACES_DIR = 'knownFaces'
-
-# Variables
-known_faces = []
-known_names = []
-
-# Process files
-for directory in os.listdir(KNOWN_FACES_DIR):
-    for file in os.listdir(f'{KNOWN_FACES_DIR}/{directory}'):
-        known_faces.append(f'{KNOWN_FACES_DIR}/{directory}/{file}')
-        known_names.append(directory)
+    video.release()
+    cv2.destroyAllWindows()
 
 # From photo
-for _ in range(len(known_faces)):
-    image = cv2.imread(known_faces[_])
+def photo_detection():
+    KNOWN_FACES_DIR = 'knownFaces'
 
-    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Variables
+    known_faces = []
+    known_names = []
 
-    faces = face_cascade.detectMultiScale(
-        grey,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30,30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+    # Process files
+    for directory in os.listdir(KNOWN_FACES_DIR):
+        for file in os.listdir(f'{KNOWN_FACES_DIR}/{directory}'):
+            known_faces.append(f'{KNOWN_FACES_DIR}/{directory}/{file}')
+            known_names.append(directory)
 
-    # Highlight faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # Display photo
-    cv2.imshow('Photo', image)
-    
-    # Exit condition
-    cv2.waitKey(0)
+    for _ in range(len(known_faces)):
+        image = cv2.imread(known_faces[_])
 
-cv2.destroyAllWindows
+        grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            grey,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30,30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Highlight faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display photo
+        cv2.imshow('Photo', image)
+        
+        # Exit condition
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows
