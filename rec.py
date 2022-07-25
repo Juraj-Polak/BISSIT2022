550eab1a92b0d492ba5335395930c074f3f80bac
CynthiaGichuki <gichukicynthia1@gmail.com>
2022-07-25 17:28:52 +0300

    face recognition packages

---
+++ rec.py
@@ -0,0 +1,12 @@
+import dlib, cv2,os
+import matplotlib.pyplot as plt
+import numpy as np
+from imutils.face_utils import FaceAligner
+
+#pose_predictor=dlib.shape_predictor(‘shape_predictor_68_face_landmarks.dat’)
+#fa = FaceAligner(pose_predictor)
+#face_encoder=dlib.face_recognition_model_v1(‘dlib_face_recognition_resnet_model_v1.dat’)
+#detector = dlib.get_frontal_face_detector()
+#modelFile = ‘opencv_face_detector_uint8.pb’
+#configFile = ‘opencv_face_detector.pbtxt’
+#net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
