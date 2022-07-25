48e3f37747fe877ebf6abea7d13bf8149f997c9c
Juraj Polák <xpolak40@vutbr.cz>
2022-07-25 21:26:11 +0200

    Attendance marking added

--- fr_modul.py
+++ fr_modul.py
@@ -32,6 +32,21 @@ def encode_faces(image_list):
     return encodings
 
 
+# Take attendance
+def take_attendance(name):
+    name_list = []
+    with open('attendance.csv', 'r+') as file:
+        data = file.readlines()
+        for line in data:
+            text = line.split(',')
+            name_list.append(text[0])
+        
+        if name not in name_list:
+            time = datetime.now()
+            time = time.strftime('%H:%M:%S')
+            file.writelines(f'\n[{name},{time}')
+
+
 # From video feed
 def video_detection():
     encoded_known = encode_faces(images)
@@ -41,27 +56,13 @@ def video_detection():
         # Frame by frame capture
         _, frames = video.read()
 
-        # # Convertion to greyscale
-        # grey = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-
-        # faces = face_cascade.detectMultiScale(
-        #     grey,
-        #     scaleFactor=1.2,
-        #     minNeighbors=4,
-        #     minSize=(30,30),
-        #     flags=cv2.CASCADE_SCALE_IMAGE
-        # )
-
-        # # Highlight faces
-        # for (x, y, w, h) in faces:
-        #     cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
         # Convertion to RGB
         frames = cv2.cvtColor(frames, cv2.COLOR_BGR2RGB)
+        frames_small = cv2.resize(frames, (0,0), None, 0.25, 0.25)
 
         # Encode faces from video
-        faces = face_recognition.face_locations(frames)
-        encoded_unknown = face_recognition.face_encodings(frames, faces)
+        faces = face_recognition.face_locations(frames_small)
+        encoded_unknown = face_recognition.face_encodings(frames_small, faces)
 
         # Compare found faces with known faces
         for encode_face, face_location in zip(encoded_unknown, faces):
@@ -72,8 +73,12 @@ def video_detection():
             # Higlight faces
             if matches[match_index]:
                 name = names[match_index]
+                take_attendance(name)
+            else:
+                name = 'unknown'
 
-            y1, x2, y2, x1 = faces
+            y1, x2, y2, x1 = face_location
+            x1, x2, y1, y2 = x1*4, x2*4, y1*4, y2*4
             cv2.rectangle(frames, (x1, y1), (x2, y2), (0, 255, 0), 2)
             
             # Show name
@@ -90,30 +95,29 @@ def video_detection():
     video.release()
     cv2.destroyAllWindows()
 
+
 # From photo
 def photo_detection():
-    KNOWN_FACES_DIR = 'knownFaces'
+    KNOWN_FACES_DIR = './test'
 
     # Variables
     known_faces = []
-    known_names = []
 
     # Process files
-    for directory in os.listdir(KNOWN_FACES_DIR):
-        for file in os.listdir(f'{KNOWN_FACES_DIR}/{directory}'):
-            known_faces.append(f'{KNOWN_FACES_DIR}/{directory}/{file}')
-            known_names.append(directory)
+    for file in os.listdir(f'{KNOWN_FACES_DIR}'):
+        known_faces.append(f'{KNOWN_FACES_DIR}/{file}')
 
 
     for _ in range(len(known_faces)):
         image = cv2.imread(known_faces[_])
+        image = cv2.resize(image, (0,0), None, 0.25, 0.25)
 
         grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(
             grey,
-            scaleFactor=1.1,
-            minNeighbors=5,
+            scaleFactor=1.3,
+            minNeighbors=3,
             minSize=(30,30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
